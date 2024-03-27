9587fb74e0a6599d0f46e499f99e1cb3dd2b7288
Rey Halsall <reduhq25@gmail.com>
2024-03-27 17:27:56 -0600

    Styling the first section

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,9 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'BookStoreAPI Docs',
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: 'https://github.com/reduhq',
 			},
 			sidebar: [
 				{
